61a36884e5284c57a24be8c8c9d5584e02e3c56c
Judy McGoldrick <jjeanius@verizon.net>
2018-02-09 01:43:17 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,7 +67,9 @@ end
 
     if (1..Song.all.length).include?(input)
       song = Song.all.sort_by(&:name)[input - 1]
-    end
       puts "Playing #{song.name} by #{song.artist.name}"
+    end
   end
+
+
 end
